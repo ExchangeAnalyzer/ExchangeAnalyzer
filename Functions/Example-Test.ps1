16b4fd34a0e21ad7d24595ec70e1e1ec01d7182a
Paul Cunningham <paul@locklan.com.au>
2015-12-27 22:07:39 +1000

    Added test EXSRV002/1
    Added test EXSRV002 but swapped numbers for EXSRV001 and 002 to make
    more sense.

--- Functions/Example-Test.ps1
+++ Functions/Example-Test.ps1
@@ -26,7 +26,7 @@ $ExchangeAnalyzerTests = @($TestsFile.Tests)
 #...................................
 
 #This function rolls up test results into an object
-Function Get-TestResultObject($TestID, $PassedList, $FailedList)
+Function Get-TestResultObject($TestID, $PassedList, $FailedList, $ErrorList)
 {
     Write-Verbose "Rolling test result object for $TestID"
      
@@ -62,17 +62,19 @@ Function Get-TestResultObject($TestID, $PassedList, $FailedList)
 Function Run-TESTID()
 {
     $TestID = "TESTID"
-    Write-Verbose "Starting test $TestID"
+    Write-Verbose "----- Starting test $TestID"
 
     $PassedList = @()
     $FailedList = @()
+    $ErrorList = @()
 
     #Your test logic goes here and populates the results
     $PassedList += "Foo"
     $FailedList += "Bar" #An empty failed list will result in a test passing.
+    #Add any errors to $ErrorList
 
     #Roll the object to be returned to the results
-    $ReportObj = Get-TestResultObject $TestID $PassedList $FailedList
+    $ReportObj = Get-TestResultObject $TestID $PassedList $FailedList $ErrorList
 
     return $ReportObj
 }
